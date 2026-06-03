5e5995e3640183c30d5d33383e15c2937acdc704
Diplom <ko-23-400@turan-edu.kz>
2026-06-03 15:49:28 +0500

    disable firebase for deploy

--- gateway/firebase.js
+++ gateway/firebase.js
@@ -1,10 +1,8 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-const db = admin.firestore();
+// Firebase отключён для деплоя — логи хранятся в памяти сервера
+const db = {
+    collection: () => ({
+        add: async () => {}
+    })
+};
 
 module.exports = { db };
